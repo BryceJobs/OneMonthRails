a42bd9d97b8a6949bc8ff9aac8dfbe68e52f1530
Brice Jobs <Brice892007@yahoo.fr>
2014-11-19 12:36:40 +0530

    starting OneMonthRailsTutorial
    The turorial on OneMonthRails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OneMonthRails::Application.config.secret_key_base = 'a4b969d3b66b629f9617d1749c6b7441303937a24b93d3521680e9906a37f8fad8fb16f983bfe85a779c378089d52b57215c02379a8ef962546720cd6419b27d'
